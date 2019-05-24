5ef328760dc8457afbee1120d1161c8bedc0a3e2
Carmen Lee <mailtocarmenlee@gmail.com>
2019-05-24 14:41:10 +0200

    fixed float input shit

--- main.c
+++ main.c
@@ -5,10 +5,10 @@
 #include <sys/time.h>
 
 int check_sqrt(int n);
-int load_input(int** A, int** B, char *filename);
-void write_output(int n, int* array, char* filename);
-void print_matrix(int* A, int n);
-void mmultiply(int* a, int *b, int *c, int n);
+int load_input(double** A, double** B, char *filename);
+void write_output(int n, double* array, char* filename);
+void print_matrix(double* A, int n);
+void mmultiply(double* a, double *b, double *c, int n);
 
 int main(int argc, char *argv[]) {
 
@@ -23,8 +23,8 @@ int main(int argc, char *argv[]) {
   int i, j, k, n;               /* n - size of the matrix */
   int s;                        /* number of shifts */
   int chunk;                    /* Amount of work each processor will do */
-  int *A, *B, *C;               /* matrix A, B, and C */
-  int *a, *b, *c;               /* submatrix A, B, C and tmp */
+  double *A, *B, *C;               /* matrix A, B, and C */
+  double *a, *b, *c;               /* submatrix A, B, C and tmp */
   int d[2];                     /* dimensions of the processors */    
   int periods[2];               /* determine whether the topology is periodic */
   int coord[2];                 /* coordinates of the cartesian topology */ 
@@ -47,7 +47,7 @@ int main(int argc, char *argv[]) {
   d[0] = (int) sqrt((float)size); d[1] = (int) sqrt((float) size);
   periods[0] = 1; periods[1] = 1;
   
-   /* Dimensions of the processors */
+  /* Dimensions of the processors */
   //MPI_Dims_create(size, 2, d);    /* did not work with n=9...*/   
   //printf("Dimension of PEs: (%d, %d)\n", d[0], d[1]);                                   
   
@@ -76,11 +76,11 @@ int main(int argc, char *argv[]) {
     /* chunk size of the matrix in each process */
     chunk = n/d[0];
 
-    // print_matrix(A, n);
-    // print_matrix(B, n);
+    print_matrix(A, n);
+    print_matrix(B, n);
 
     // printf("Dimensions of the processors: %d, %d\n", d[0], d[1]);
-    C = (int *) malloc(n*n*sizeof(int));
+    C = (double *) malloc(n*n*sizeof(double));
   }
   
   
@@ -91,14 +91,14 @@ int main(int argc, char *argv[]) {
   MPI_Datatype matrix;
   MPI_Datatype array;
 
-  MPI_Type_vector(chunk, chunk, n, MPI_INT, &array);
-  MPI_Type_create_resized(array, 0, sizeof(int), &matrix);
+  MPI_Type_vector(chunk, chunk, n, MPI_DOUBLE, &array);
+  MPI_Type_create_resized(array, 0, sizeof(double), &matrix);
   MPI_Type_commit(&matrix);
 
   /* Local sub matrix memloc */
-  a   = (int*) malloc(chunk * chunk * sizeof(int));
-  b   = (int*) malloc(chunk * chunk * sizeof(int));
-  c   = (int*) calloc(chunk * chunk, sizeof(int));
+  a   = (double*) malloc(chunk * chunk * sizeof(double));
+  b   = (double*) malloc(chunk * chunk * sizeof(double));
+  c   = (double*) calloc(chunk * chunk, sizeof(double));
 
   int counts[d[0] * d[1]];
   int displs[d[0] * d[1]];
@@ -120,8 +120,8 @@ int main(int argc, char *argv[]) {
 //   printf("\n");
 
   /* Scatter A and B to the processors */
-  MPI_Scatterv(A, counts, displs, matrix, a, chunk*chunk, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Scatterv(B, counts, displs, matrix, b, chunk*chunk, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Scatterv(A, counts, displs, matrix, a, chunk*chunk, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Scatterv(B, counts, displs, matrix, b, chunk*chunk, MPI_DOUBLE, 0, MPI_COMM_WORLD);
   MPI_Barrier(MPI_COMM_WORLD);
   
   /* print and inspect */
@@ -146,10 +146,10 @@ int main(int argc, char *argv[]) {
   
   /* Alignment */
   MPI_Cart_shift(comm_cart, 1, -coord[0], &alignsrc, &aligndes);  /* E or W based on i */
-  MPI_Sendrecv_replace(a, chunk*chunk, MPI_INT, aligndes, 11, alignsrc, 11, comm_cart, &status);
+  MPI_Sendrecv_replace(a, chunk*chunk, MPI_DOUBLE, aligndes, 11, alignsrc, 11, comm_cart, &status);
 
   MPI_Cart_shift(comm_cart, 0, -coord[1], &alignsrc, &aligndes);  /* N or S based on j */
-  MPI_Sendrecv_replace(b, chunk*chunk, MPI_INT, aligndes, 11, alignsrc, 11, comm_cart, &status);
+  MPI_Sendrecv_replace(b, chunk*chunk, MPI_DOUBLE, aligndes, 11, alignsrc, 11, comm_cart, &status);
 
   /* print and inspect */
   // printf("Rank %d has a and b: \n", rank);
@@ -163,15 +163,15 @@ int main(int argc, char *argv[]) {
 
   /* shift and compute */
   for (i=0; i<d[0]-1; i++) {
-      MPI_Sendrecv_replace(a, chunk*chunk, MPI_INT, left, 11, right, 11, comm_cart, &status);
-      MPI_Sendrecv_replace(b, chunk*chunk, MPI_INT, up, 11, down, 11, comm_cart, &status);
+      MPI_Sendrecv_replace(a, chunk*chunk, MPI_DOUBLE, left, 11, right, 11, comm_cart, &status);
+      MPI_Sendrecv_replace(b, chunk*chunk, MPI_DOUBLE, up, 11, down, 11, comm_cart, &status);
       mmultiply(a, b, c, chunk);
     //   printf("Rank %d has c: \n", rank); 
     //   print_matrix(c, chunk);
    }
 
   /* Gather C from the processors to master */
-  MPI_Gatherv(c, chunk*chunk, MPI_INT, C, counts, displs, matrix, 0, MPI_COMM_WORLD);
+  MPI_Gatherv(c, chunk*chunk, MPI_DOUBLE, C, counts, displs, matrix, 0, MPI_COMM_WORLD);
   MPI_Barrier(MPI_COMM_WORLD);
 
   /* Inspect the result */
@@ -207,47 +207,47 @@ int main(int argc, char *argv[]) {
 }
 
 
-int load_input(int** A, int** B, char *filename) {
+int load_input(double** A, double** B, char *filename) {
   FILE *fp = fopen(filename, "r");
   int n;
   if (!fp) {
     printf("load_data error: failed to open input file '%s'.\n", filename);
     return -1;
   }
-  fscanf(fp, "%d", &n);
-  *A = malloc(sizeof(int) * n * n);
-  *B = malloc(sizeof(int) * n * n);
+  fscanf(fp, "%d ", &n);
+  *A = malloc(sizeof(double) * n * n);
+  *B = malloc(sizeof(double) * n * n);
   int i;
   for (i=0; i<n*n; i++) {
-      fscanf(fp, "%d", &((*A)[i]));
+      fscanf(fp, "%lf ", &((*A)[i]));
   }
   for (i=0; i<n*n; i++) {
-      fscanf(fp, "%d", &((*B)[i]));
+      fscanf(fp, "%lf ", &((*B)[i]));
   }
   fclose(fp);
   return n;
 }
 
-void write_output(int n, int* array, char* filename) {
+void write_output(int n, double* array, char* filename) {
   FILE *fp = fopen(filename, "w");
   int i;
   for (i=0; i<n*n; i++) {
-      fprintf(fp, "%.6f ", (float) array[i]);
+      fprintf(fp, "%.6f ", array[i]);
       if ((i+1)%n == 0) fprintf(fp, "\n");
   }
   fclose(fp);
 }
 
-void print_matrix(int* A, int n){
-      int i,j;
+void print_matrix(double* A, int n){
+    int i,j;
     for (i=0; i<n*n; i++) {
-        printf("%d ", A[i]);
+        printf("%f ", A[i]);
         if ((i+1)%n == 0) printf("\n");
     }
     printf("\n");
 }
 
-void mmultiply(int* a, int *b, int *c, int n) {
+void mmultiply(double* a, double *b, double *c, int n) {
     int i, j, k;
     for (i=0; i<n; i++) {
         for (j=0; j<n; j++) {
